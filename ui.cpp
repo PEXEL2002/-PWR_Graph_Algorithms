58614661773574a9ecf4a8d4ab6d2e5af53e6270
Bartlomiej Kuk <bartek.kuk321@gmail.com>
2024-05-08 10:57:23 +0200

    chore: Add function to generate graph data

---
+++ ui.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <random>
+#include <string>
+#include <set>
+#include <utility>
+
+void generateGraphData(int vertices, int density) {
+    std::vector<std::vector<int>> matrix(vertices, std::vector<int>(vertices, 0));
+    std::vector<std::vector<std::pair<int, int>>> list(vertices);
+    
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> weightDis(1, 100);  // Zakres wag od 1 do 100
+    
+    int maxEdges = vertices * (vertices - 1) / 2;
+    int numEdges = static_cast<int>(maxEdges * (density/100));
+    std::string matrixFileName = "matrix"+std::to_string(vertices)+std::to_string(density)+".txt";
+    std::string listFileName = "list"+std::to_string(vertices)+std::to_string(density)+".txt";
+
+    std::set<std::pair<int, int>> edges;
+    while (edges.size() < numEdges) {
+        int i = gen() % vertices;
+        int j = gen() % vertices;
+        if (i != j) {
+            if (i > j) std::swap(i, j);
+            edges.insert({i, j});
+        }
+    }
+    // Dodawanie krawędzi do macierzy i listy sąsiedztwa
+    for (const auto& edge : edges) {
+        int weight = weightDis(gen);
+        matrix[edge.first][edge.second] = weight;
+        matrix[edge.second][edge.first] = weight;
+        list[edge.first].push_back({edge.second, weight});
+        list[edge.second].push_back({edge.first, weight});
+    }
+    // Zapisywanie do pliku - macierz sąsiedztwa
+    std::ofstream matrixFile(matrixFileName);
+    if (!matrixFile) {
+        std::cerr << "Nie można otworzyć pliku: " << matrixFileName << std::endl;
+        return;
+    }
+    for (const auto& row : matrix) {
+        for (int val : row) {
+            matrixFile << val << " ";
+        }
+        matrixFile << "\n";
+    }
+    matrixFile.close();
+
+    // Zapisywanie do pliku - lista sąsiedztwa
+    std::ofstream listFile(listFileName);
+    if (!listFile) {
+        std::cerr << "Nie można otworzyć pliku: " << listFileName << std::endl;
+        return;
+    }
+    for (const auto& row : list) {
+        for (const auto& pair : row) {
+            listFile << pair.first << "(" << pair.second << ") ";
+        }
+        listFile << "\n";
+    }
+    listFile.close();
+}
---
+++ ui.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+/**
+ * @brief Generuje dane grafu o zadanej liczbie wierzchołków i gęstości
+ * @param vertices liczba wierzchołków
+ * @param density gęstość grafu w procentach
+*/
+void generateGraphData(int vertices, int density);
